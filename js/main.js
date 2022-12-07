3a2c87aa0e5dfd7f3f9eb13c38feaef47ae4a12e
Alejandro Tello <alee.e.tello@gmail.com>
2022-12-07 17:09:47 -0300

    Modifico Funcionalidades

--- js/main.js
+++ js/main.js
@@ -26,27 +26,27 @@ const renderizarAnimales = (especie) => {
         let edad = inputFiltroEdad.value 
 
         if(edad && sexo) {
-            console.log('AMOBS');
             return especie.includes(animal.especie) && edad === animal.edad  && sexo === animal.sexo
 
         } else if(edad && !sexo){
-            console.log('Solo Edad');
             return especie.includes(animal.especie) && edad === animal.edad
 
         } else if(!edad && sexo) {
             return especie.includes(animal.especie) && sexo === animal.sexo
 
         } else {
-            console.log('ELSE');
             return especie.includes(animal.especie)
         }
         })
     containerCards.innerHTML = ''
     
     especieEnAdopcion.forEach((animal) => {
-        const {id, nombre, pelaje, edad, sexo, comportamiento, especie, img, descripcion} = animal
+        const {id, nombre, pelaje, edadDetalle, sexo, comportamiento, especie, img} = animal
         const cardAnimal = document.createElement('div')
-      
+        const simboloSexo = sexo == 'Macho'
+            ? '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-gender-male" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M9.5 2a.5.5 0 0 1 0-1h5a.5.5 0 0 1 .5.5v5a.5.5 0 0 1-1 0V2.707L9.871 6.836a5 5 0 1 1-.707-.707L13.293 2H9.5zM6 6a4 4 0 1 0 0 8 4 4 0 0 0 0-8z"/></svg>'
+            :'<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-gender-female" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M8 1a4 4 0 1 0 0 8 4 4 0 0 0 0-8zM3 5a5 5 0 1 1 5.5 4.975V12h2a.5.5 0 0 1 0 1h-2v2.5a.5.5 0 0 1-1 0V13h-2a.5.5 0 0 1 0-1h2V9.975A5 5 0 0 1 3 5z"/></svg>'
+
         cardAnimal.classList.add('card', 'animalCard', 'text-light', 'm-3')
         cardAnimal.setAttribute('data-aos', 'fade-up')
         cardAnimal.setAttribute('data-aos-duration', '1000')
@@ -55,16 +55,14 @@ const renderizarAnimales = (especie) => {
             <img src="${img}" alt="${nombre} ${especie} ${pelaje}">
             </div>
             <div class="card-body">
-            <h5 class="card-title fs-3">${nombre} <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-gender-male" viewBox="0 0 16 16">
-            <path fill-rule="evenodd" d="M9.5 2a.5.5 0 0 1 0-1h5a.5.5 0 0 1 .5.5v5a.5.5 0 0 1-1 0V2.707L9.871 6.836a5 5 0 1 1-.707-.707L13.293 2H9.5zM6 6a4 4 0 1 0 0 8 4 4 0 0 0 0-8z"/>
-            </svg></h5>
-            <p class="card-text m-0"><span class="fw-bold">Edad:</span> ${edad}</p>
+            <h5 class="card-title fs-3">${nombre} ${simboloSexo}</h5>
+            <p class="card-text m-0"><span class="fw-bold">Edad:</span> ${edadDetalle}</p>
             <p class="card-text m-0"><span class="fw-bold">Sexo:</span> ${sexo}</p>
             <p class="card-text m-0"><span class="fw-bold">Color:</span> ${pelaje}</p>
             <p class="card-text m-0"><span class="fw-bold">Comportamiento:</span> ${comportamiento}</p>
             <a href="#" class="masInfo text-decoration-none text-light">+Mas Info...</a>
             
-                <button class="btn btn-primary my-3 col-12 btnAdopcion" type="button" >Quiero adoptar</button>
+                <button class="btn btn-success my-3 col-12 btnAdopcion" type="button" >Quiero adoptar</button>
                 
             </div>`
 
@@ -103,6 +101,7 @@ const renderizarAnimales = (especie) => {
 selectorEspecie.forEach((especie) => {
 especie.addEventListener('click', () => {
     containerCards.innerHTML = ''
+    filtros.reset()
     setTimeout(() => {
 
         fetch("../json/data.json")
@@ -140,6 +139,7 @@ const validarContrato = () => {
     form.addEventListener('submit', (e) => {      
         e.preventDefault()
 
+        const edad = document.querySelector('#inputEdad').value
         if(edad >= 18) {
             Swal.fire(
                 '¡Contrato enviado!',
@@ -179,19 +179,36 @@ if(continuarAdopcion && !location.href.includes('contrato.html')) {
             localStorage.removeItem("adoptado");
         }
       })
-} 
 
-if(location.href.includes('adopciones.html')){
+} else if(location.href.includes('adopciones.html')){
 
-    btnFiltrar.addEventListener('click', () => {renderizarAnimales(especieSeleccionada)})
-
-    btnBorrarFiltro.addEventListener('click', () => {
+    btnFiltrar.addEventListener('click', (e) => {
+        e.preventDefault()
+        renderizarAnimales(especieSeleccionada)
+      })
+    
+      btnBorrarFiltro.addEventListener('click', (e) => {
+        e.preventDefault()
         filtros.reset()
-        renderizarAnimales(especieSeleccionada)})
+        renderizarAnimales(especieSeleccionada)
+      })
 
-}
+} else if(location.href.includes('contrato.html') && !continuarAdopcion){
 
-if(location.href.includes('contrato.html')){
+    Swal.fire({
+        title: 'Ups',
+        text: "Para completar el contrato primero debes elegir uno de nuestros rescatados. Serás redirigido a la página de adopciones",
+        icon: 'warning',
+        confirmButtonColor: '#3085d6',
+        confirmButtonText: 'Entendido'
+        }).then((result) => {
+        if (result.isConfirmed) {
+            location.href = "../pages/adopciones.html"
+        }
+        })
+
+} else if (location.href.includes('contrato.html') && continuarAdopcion) {
+    
     validarContrato()
     mostrarDatos()
 
@@ -208,7 +225,6 @@ if(location.href.includes('contrato.html')){
             denyButtonText: 'Cancelar adopción'
           }).then((result) => {
             if (result.isDenied) {
-                console.log("LASDLALSD");
                 localStorage.removeItem("adoptado");
                 location.href = "../pages/adopciones.html"
             }
